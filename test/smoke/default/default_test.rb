5a171e6518f6dda65f79ba4bb7cc9b52b2f357df
Yangkai Lyu <lyk119@gmail.com>
2017-08-02 00:37:16 -0400

    test1

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -16,3 +16,24 @@ end
 describe port(80), :skip do
   it { should be_listening }
 end
+
+['openjdk-6-jdk', 'httpd'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe service('httpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/var/www/html/index.html') do
+  it { should exist }
+  it { should be_file }
+  its('content') { should match /[Hh]ello [Ww]orld!/ }
+end
+
+describe command('curl http://localhost') do
+  its('stdout') { should match /[Hh]ello [Ww]orld!/ }
+end
